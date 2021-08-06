68a020a5e09e8a64973f1510c9cde9788118256e
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-08-06 17:36:13 -0300

    Database Exception para o delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -2,15 +2,14 @@ package com.educandoweb.course.resources.exceptions;
 
 import java.time.Instant;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -20,6 +19,15 @@ public class ResourceExceptionHandler {
 	ResponseEntity<StandardError> resourceNotFound(HttpServletRequest re, ResourceNotFoundException e){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), re.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	ResponseEntity<StandardError> databaseException(HttpServletRequest re, DatabaseException e){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), re.getRequestURI());
 		return ResponseEntity.status(status).body(err);
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,28 +18,35 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> op = repository.findById(id);
 		return op.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User saveUser(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void deleteUser(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
-	//getOne apenas pega o objeto monitorado e depois mexe no banco, o findBy pega no banco
+
+	// getOne apenas pega o objeto monitorado e depois mexe no banco, o findBy pega
+	// no banco
 	public User updateUser(Long id, User user) {
 		User obj = repository.getOne(id);
-		updateData(obj,user);
+		updateData(obj, user);
 		return repository.save(obj);
 	}
 
@@ -46,6 +56,5 @@ public class UserService {
 		obj.setName(user.getName());
 		obj.setPhone(user.getPhone());
 	}
-	
-	
+
 }
